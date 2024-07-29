75c955e986aa2ecdab4854181f9c7445b13436c6
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-07-29 23:53:06 +0700

    feat: add main screen on root

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import Cards from "./components/Cards/Cards";
-
 import "./App.css";
+import Main from "./screens/Main";
 
 function App() {
   return (
-    <div className="flex p-8">
-      <Cards
-        style={{
-          flexDirection: "column",
-        }}
-      >
-        <h1>This is a Card component</h1>
-        <h1>This is a Card component</h1>
-        <h1>This is a Card component</h1>
-        <h1>This is a Card component</h1>
-        <br />
-        <h1>This is a Card component</h1>
-        <h1>This is a Card component</h1>
-        <h1>This is a Card component</h1>
-        <h1>This is a Card component</h1>
-      </Cards>
+    <div className="pb-8">
+      <Main />
     </div>
   );
 }
