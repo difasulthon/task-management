dd92c8923d4ef6102b9e677897561c966f56523b
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-07-29 23:52:26 +0700

    feat: add custom color for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          primary: '#FD7B38',
+          secondary: '#FFF4EE',
+          greyPrimary: '#65676D',
+          greySecondary: '#F1F2F4',
+          redPrimary: '#FF6C65'
+        }
+      }
+    },
   },
   plugins: [],
 }
