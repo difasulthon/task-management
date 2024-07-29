de8cf9347aa01b215b784e2ccb1537c4f0efc19f
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-07-29 23:51:27 +0700

    feat: add card label component

---
+++ src/components/CardLabel/CardLabel.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type Props = {
+  label: string;
+  isSolid: boolean;
+};
+
+const CardLabel = (props: Props) => {
+  const { label, isSolid } = props;
+
+  if (isSolid) {
+    return (
+      <div className="flex rounded-md px-2 py-1 bg-custom-greySecondary">
+        <p className="text-custom-greyPrimary text-xs">{label}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex rounded-md px-2 py-1 bg-white border border-custom-redPrimary">
+      <p className="text-custom-redPrimary text-xs">{label}</p>
+    </div>
+  );
+};
+
+export default CardLabel;
