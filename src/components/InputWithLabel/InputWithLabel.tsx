811e776e70b96d7eb5692a1d9b6df6c0a7501974
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-07-31 17:30:00 +0700

    feat: add InputWithLabel component

---
+++ src/components/InputWithLabel/InputWithLabel.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Input, { Props as InputProps } from "../Input/Input";
+
+type Props = InputProps & {
+  label: string;
+};
+
+const InputWithLabel = (props: Props) => {
+  const { label, type, onChange, placeholder, value } = props;
+
+  return (
+    <div className="flex flex-col">
+      <p className="font-medium text-md mb-1">{label}</p>
+      <Input
+        type={type}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+      />
+    </div>
+  );
+};
+
+export default InputWithLabel;
