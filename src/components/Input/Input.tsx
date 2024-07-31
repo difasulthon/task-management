69001e53189e876f812b795c212d5e0678a869a2
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-07-31 17:29:29 +0700

    feat: update input component, placeholder to be dynamic, add onFocus and onBlur

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,21 +1,36 @@
 import React, { ChangeEvent, HTMLInputTypeAttribute } from "react";
 
-type Props = {
+export type Props = {
   type: HTMLInputTypeAttribute;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
   value: string;
+  placeholder: string;
 };
 
 const Input = (props: Props): React.JSX.Element => {
-  const { type, onChange, value } = props;
+  const { type, onChange, value, placeholder } = props;
+
+  const [isFocused, setIsFocused] = React.useState<boolean>(false);
+
+  const onFocusHandler = () => {
+    setIsFocused(true);
+  };
+
+  const onBlurHandler = () => {
+    setIsFocused(false);
+  };
 
   return (
     <input
       type={type}
       value={value}
       onChange={onChange}
-      className="border-black bg-white border rounded-lg py-2 pl-2 w-80 font-normal text-sm"
-      placeholder="Search task"
+      onFocus={onFocusHandler}
+      onBlur={onBlurHandler}
+      className={`bg-white border rounded-lg py-2 pl-2 w-80 font-normal text-sm 
+        ${isFocused ? "border-custom-primary" : "border-black"}
+      `}
+      placeholder={placeholder}
     />
   );
 };
