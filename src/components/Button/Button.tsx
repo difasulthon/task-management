6439b5f3a306fa079c1b510fa3151eca012f9152
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-08-01 22:58:39 +0700

    feat: update Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,18 +4,30 @@ type Props = {
   icon: React.JSX.Element;
   label: string;
   onClick: void;
+  width?: string;
 };
 
 const Button = (props: Props): React.JSX.Element => {
-  const { icon, label, onClick } = props;
+  const { icon, label, onClick, width } = props;
+
+  if (icon) {
+    return (
+      <button
+        onClick={onClick}
+        className="flex flex-row items-center bg-custom-greenPrimary rounded-lg px-2 hover:opacity-50"
+      >
+        {icon}
+        <p className="text-sm text-white ml-2 font-semibold">{label}</p>
+      </button>
+    );
+  }
 
   return (
     <button
       onClick={onClick}
-      className="flex flex-row items-center bg-custom-greenPrimary rounded-lg px-2 hover:opacity-50"
+      className={`flex justify-center bg-custom-greenPrimary rounded-lg px-2 py-2 hover:opacity-50 ${width}`}
     >
-      {icon}
-      <p className="text-sm text-white ml-2 font-semibold">{label}</p>
+      <p className="text-sm text-white font-semibold">{label}</p>
     </button>
   );
 };
