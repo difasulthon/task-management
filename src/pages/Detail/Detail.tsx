4ae19be5fb0ed4c17158483ceb060dc6318ae21d
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-08-08 23:36:30 +0700

    feat: add useReducer and modify ui component on Detail page

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,39 +1,70 @@
 import React from "react";
 
+import {
+  initialInputFlagStates,
+  inputFlagReducer,
+} from "./reducers/InputFlagReducer";
+import type { ActionType, InputFlagState } from "./Detail.types";
+import InputTitle from "./components/InputTitle";
+import InputDescription from "./components/InputDescription";
+import DropdownStatus from "./components/DropdownStatus";
+import DropdownLabel from "./components/DropdownLabel";
+import DropdownPriority from "./components/DropdownPriority";
+
 const Detail = (): React.JSX.Element => {
+  const [state, dispatch] = React.useReducer<{
+    state: InputFlagState;
+    action: ActionType;
+  }>(inputFlagReducer, initialInputFlagStates);
+
   return (
     <div className="flex w-12/12 justify-center">
       <div className="flex flex-row gap-3 pt-10 w-7/12">
         <div className="flex flex-col max-w-xl">
-          <h3 className="font-medium text-2xl mb-12">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio
-            dolore architecto voluptatem animi nostrum asperiores.
-          </h3>
-          <p className="font-normal text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi optio
-            dolore architecto voluptatem animi nostrum asperiores.
-          </p>
+          <InputTitle state={state} dispatch={dispatch} onChange={() => {}} />
+          <InputDescription
+            state={state}
+            dispatch={dispatch}
+            onChange={() => {}}
+          />
         </div>
         <div className="flex flex-col">
-          <div className="flex bg-custom-bluePrimary h-8 items-center px-4 py-3 rounded-md gap-1">
-            <p className="text-white text-base">IN PROGRESS</p>
-            <box-icon name="chevron-down" color="white" size="sm"></box-icon>
-          </div>
-          <div className="mt-9 border-2 border-custom-greySecondary py-2 rounded-md">
+          <DropdownStatus
+            state={state}
+            dispatch={dispatch}
+            onChange={() => {}}
+          />
+          <div className="mt-9 border-2 border-custom-greySecondary py-2 rounded-md pr-2">
             <div className="mb-2">
               <p className="text-black font-semibold text-sm ml-2">Details</p>
               <div className="border-b-2 border-custom-greySecondary mt-1"></div>
             </div>
             <div className="mb-3 ml-2">
               <p className="text-black font-normal text-sm">Label</p>
-              <p className="text-custom-bluePrimary font-medium text-sm">
-                Daily Task
+              <DropdownLabel
+                state={state}
+                dispatch={dispatch}
+                onChange={() => {}}
+              />
+            </div>
+            <div className="mb-3 ml-2">
+              <p className="text-black font-normal text-sm">Priority</p>
+              <DropdownPriority
+                state={state}
+                dispatch={dispatch}
+                onChange={() => {}}
+              />
+            </div>
+            <div className="mb-3 ml-2">
+              <p className="text-black font-normal text-sm">Created at</p>
+              <p className="text-gray-500 font-medium text-sm">
+                August 07, 2024
               </p>
             </div>
             <div className="ml-2">
-              <p className="text-black font-normal text-sm">Priority</p>
-              <p className="text-custom-redPrimary font-medium text-sm">
-                High Priority
+              <p className="text-black font-normal text-sm">Last modified</p>
+              <p className="text-gray-500 font-medium text-sm">
+                August 07, 2024
               </p>
             </div>
           </div>
