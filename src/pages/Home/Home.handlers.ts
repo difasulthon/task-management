574d2145cecb0b6c78866cca81300a992326d25b
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-08-12 23:13:04 +0700

    feat: add handler for generate search result and click right icon on input type

--- src/pages/Home/Home.handlers.ts
+++ src/pages/Home/Home.handlers.ts
@@ -1,9 +1,54 @@
+import Constants from "../../constants";
+import type { Task } from "../../types/Task.type";
+import { getTasks } from "../../utils/Storage";
+
+const {ROUTES} = Constants
+
 const getGenerateList = (tasks: Array<object>, filterId: number) => {
-  const result = tasks.filter(item => item.status.id === filterId)
+  const result = tasks
+    .filter(item => item.status.id === filterId)
+    .sort((a, b) => a.title.localeCompare(b.title))
 
   return result;
 }
 
+const generateSearchTask = (value: string) => {
+  const keyWord = value.toLowerCase();
+
+  const getTaskByKeyword: Array<Task> = getTasks().filter((item: Task) => {
+    const title = item.title.toLowerCase();
+    const description = item.description.toLowerCase();
+    const status = item.status.label.toLowerCase();
+    const label = item.label.label.toLowerCase();
+    const priority = item.priority.label.toLowerCase();
+    const taskNumber = "tmon-" + item.taskNumber.toString();
+
+    return (
+      title.includes(keyWord) ||
+      description.includes(keyWord) ||
+      status.includes(keyWord) ||
+      label.includes(keyWord) ||
+      priority.includes(keyWord) ||
+      taskNumber.includes(keyWord)
+    );
+  });
+
+  return getTaskByKeyword;
+};
+
+const handleOnClickRightIcon = (setKeyWord) => {
+  setKeyWord("");
+};
+
+const onNewClicked = (navigate) => () => navigate(ROUTES.NEW_TASK);
+
+const onTaskItemClicked = (navigate, id) => () =>
+  navigate(`${ROUTES.TASK}/${id}`);
+
 export {
-  getGenerateList
+  getGenerateList,
+  handleOnClickRightIcon,
+  generateSearchTask,
+  onNewClicked,
+  onTaskItemClicked
 }
