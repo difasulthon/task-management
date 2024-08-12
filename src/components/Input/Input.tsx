7d11170a945c8a2d3dc96aa36faf876ea5d28279
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-08-12 23:11:59 +0700

    feat: add right icon on Input component for search input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -7,10 +7,21 @@ export type Props = {
   placeholder: string;
   isArea: boolean;
   required: boolean;
+  rightIcon: React.ReactNode;
+  onClickRightIcon: void;
 };
 
 const Input = (props: Props): React.JSX.Element => {
-  const { type, onChange, value, placeholder, isArea, required } = props;
+  const {
+    type,
+    onChange,
+    value,
+    placeholder,
+    isArea,
+    required,
+    rightIcon,
+    onClickRightIcon,
+  } = props;
 
   const [isFocused, setIsFocused] = React.useState<boolean>(false);
 
@@ -40,17 +51,25 @@ const Input = (props: Props): React.JSX.Element => {
   }
 
   return (
-    <input
-      type={type}
-      value={value}
-      onChange={onChange}
-      onFocus={onFocusHandler}
-      onBlur={onBlurHandler}
-      className={`bg-white border rounded-lg py-2 pl-2 w-80 font-normal text-sm 
+    <div className="relative">
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocusHandler}
+        onBlur={onBlurHandler}
+        className={`bg-white border rounded-lg py-2 pl-2 w-80 font-normal text-sm 
         ${isFocused ? "border-custom-primary" : "border-black"}
       `}
-      placeholder={placeholder}
-    />
+        placeholder={placeholder}
+      />
+      <div
+        className="absolute right-0 top-2 hover:cursor-pointer hover:opacity-50"
+        onClick={onClickRightIcon}
+      >
+        {value && rightIcon}
+      </div>
+    </div>
   );
 };
 
