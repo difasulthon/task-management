e7af8e2e877f988fd056c0b38f5db70edf562ceb
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-08-14 16:42:59 +0700

    feat: fixing search remove desc and status indicator

--- src/pages/Home/Home.handlers.ts
+++ src/pages/Home/Home.handlers.ts
@@ -17,16 +17,12 @@ const generateSearchTask = (value: string) => {
 
   const getTaskByKeyword: Array<Task> = getTasks().filter((item: Task) => {
     const title = item.title.toLowerCase();
-    const description = item.description.toLowerCase();
-    const status = item.status.label.toLowerCase();
     const label = item.label.label.toLowerCase();
     const priority = item.priority.label.toLowerCase();
     const taskNumber = TASK_NUMBER_NAME.toLowerCase() + "-" + item.taskNumber.toString();
 
     return (
       title.includes(keyWord) ||
-      description.includes(keyWord) ||
-      status.includes(keyWord) ||
       label.includes(keyWord) ||
       priority.includes(keyWord) ||
       taskNumber.includes(keyWord)
